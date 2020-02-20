ec30aa84079a27bc61abe89fcfba27fd9a4dae90
Sándor Bakos <bakos.webd@gmail.com>
2020-02-20 20:59:39 +0100

    js flatten

---
+++ Strings_and_arrays/flatten.js
@@ -0,0 +1,12 @@
+const arrayToFlatten = [1, 2, 3, [4, 5], [6, 7, 8, [9]]]
+function flatThis(arr, new_arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] instanceof Array) {
+      flatThis(arr[i], new_arr)
+    } else {
+      new_arr.push(arr[i])
+    }
+  }
+  return new_arr
+}
+console.log(flatThis(arrayToFlatten, []))
